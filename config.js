9361397e48485c595de4e2a51daa821ac7e236b1
iSanax <gracjan.jagiello.dev@gmail.com>
2026-08-21 09:56:12 +0200

    update

--- config.js
+++ config.js
@@ -3,14 +3,9 @@ const CONFIG = {
   developmentVersion: "2.2.0-dev.1",
 
   experimentalEnabled: true,
-
   experimentalTitle: "Automatic waypoint suggestions",
-
-  experimentalDescription:
-    "The system is designed primarily to work with content added by other mods without requiring manual configuration.",
-
-  experimentalWarning:
-    "This feature is still under development, so some targets may receive an incorrect name or icon.",
+  experimentalDescription: "The system is designed primarily to work with content added by other mods without requiring manual configuration.",
+  experimentalWarning: "This feature is still under development, so some targets may receive an incorrect name or icon.",
 
   stableDownloadId: 116291,
   developmentDownloadId: 116219
